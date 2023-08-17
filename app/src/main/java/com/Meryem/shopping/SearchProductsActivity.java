9170d2066461428f85bb6e2bfff5bb14140d0f31
Meryembarkallah21 <meryembarkallah21@gmail.com>
2023-08-17 14:48:43 +0100

    fixed

---
+++ app/src/main/java/com/Meryem/shopping/SearchProductsActivity.java
@@ -0,0 +1,93 @@
+package com.Meryem.shopping;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.Meryem.shopping.Model.Products;
+import com.Meryem.shopping.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+public class SearchProductsActivity extends AppCompatActivity {
+
+    //search mta3 produit yjik m3ah les donnes mte3ou
+    private Button searchBtn;
+    private EditText inputText;
+    private RecyclerView searchList;
+    private String searchInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_products);
+        inputText = findViewById(R.id.search_product_name);
+        searchBtn = findViewById(R.id.search_btn);
+        searchList = findViewById(R.id.search_list);
+        searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchInput = inputText.getText().toString();
+                onStart();
+
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(reference.orderByChild("pname").startAt(searchInput), Products.class)
+                .build();
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter =
+                new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull ProductViewHolder holder, int position, @NonNull final Products model) {
+                        holder.txtProductName.setText(model.getPname());
+                        holder.txtProductDescription.setText(model.getDescription());
+                        holder.txtProductPrice.setText("Price = " + model.getPrice() + "dt.");
+                        Picasso.get().load(model.getImage()).into(holder.imageView);
+                        holder.itemView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent =new Intent(SearchProductsActivity.this,ProductDetailsActivity.class);
+                                intent.putExtra("pid",model.getPid());
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+
+                    @NonNull
+                    @Override
+                    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_items_layout, parent, false);
+                        ProductViewHolder holder = new ProductViewHolder(view);
+                        return holder;
+                    }
+                };
+        searchList.setAdapter(adapter);
+        adapter.startListening();
+    }
+}
